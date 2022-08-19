8d55202c9f15266c5e61ecbf27ce567c21c11d0d
tyral <tyral@zklink.org>
2022-08-19 17:34:21 +0800

    update sdk

--- dist/conf/index.js
+++ dist/conf/index.js
@@ -1,5 +1,5 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.ZKEX_API_URL = exports.L2_HOST = void 0;
-exports.L2_HOST = 'http://39.103.171.131:3030';
+exports.L2_HOST = 'https://aws-gw-v2.zk.link/rpc';
 exports.ZKEX_API_URL = 'https://aws-test-1.zkex.com/api-v1/api';
